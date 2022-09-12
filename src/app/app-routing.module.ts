46809a1907d08df07761620c2190b9f5173b8b74
Gaurav Singh <gaurav.singh@discoverdollar.com>
2022-09-12 14:26:58 +0545

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LmsDashboardComponent } from './Components/lms-dashboard/lms-dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component : LmsDashboardComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
