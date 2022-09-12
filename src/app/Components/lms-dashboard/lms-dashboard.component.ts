61cb3336334a1587faf08bea9c635aac389cfcf0
Gaurav Singh <gaurav.singh@discoverdollar.com>
2022-09-12 14:23:09 +0545

    Created Structure of App

---
+++ src/app/Components/lms-dashboard/lms-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lms-dashboard',
+  templateUrl: './lms-dashboard.component.html',
+  styleUrls: ['./lms-dashboard.component.css']
+})
+export class LmsDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LmsDashboardComponent } from './Components/lms-dashboard/lms-dashboard.component';
+import { LoginComponent } from './Components/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LmsDashboardComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
